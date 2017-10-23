831a34910ee058c7ecf3f93553be2ff896ead923
Andres <afmappe@hotmail.com>
2017-10-22 23:58:41 -0500

    Estilos offerer home

--- src/app/home/components/offerent_home/offerent_home.controller.ts
+++ src/app/home/components/offerent_home/offerent_home.controller.ts
@@ -1,37 +1,37 @@
-  /** @ngInject */
+/** @ngInject */
 export function OfferentHomeController($scope: any,
-                                       toastr: any,
-                                       $translate: ng.translate.ITranslateService, 
-                                       $timeout : ng.ITimeoutService,
-                                       $sessionStorage : any,
-                                       $state: ng.ui.IStateService,
-                                       $http: any,
-                                       NgTableParams: any) {
+  toastr: any,
+  $translate: ng.translate.ITranslateService,
+  $timeout: ng.ITimeoutService,
+  $sessionStorage: any,
+  $state: ng.ui.IStateService,
+  $http: any,
+  NgTableParams: any) {
 
-    var vm = this;
+  var vm = this;
 
-    vm.JWTtoken = $sessionStorage.JWTtoken;
-    vm.columns = [{title:'Nombre', field: 'name'}, {title:'Edad', field: 'age'}];
-    var data = [{name: "Moroni", age: 50} ,{name: "Moroni", age: 50},{name: "Moroni", age: 50},{name: "Moroni", age: 50},{name: "Moroni", age: 50},{name: "Moroni", age: 50},{name: "Moroni", age: 50},{name: "Moroni", age: 50},{name: "Moroni", age: 50},{name: "Moroni", age: 50},{name: "Moroni", age: 50},{name: "Moroni", age: 50},{name: "Moroni", age: 50},{name: "Moroni", age: 50},{name: "Moroni", age: 50},{name: "Moroni", age: 50},{name: "Moroni", age: 50}];
+  vm.JWTtoken = $sessionStorage.JWTtoken;
+  vm.columns = [{ title: 'Nombre', field: 'name' }, { title: 'Edad', field: 'age' }];
+  var data = [{ name: "Moroni", age: 50 }, { name: "Moroni", age: 50 }, { name: "Moroni", age: 50 }, { name: "Moroni", age: 50 }, { name: "Moroni", age: 50 }, { name: "Moroni", age: 50 }, { name: "Moroni", age: 50 }, { name: "Moroni", age: 50 }, { name: "Moroni", age: 50 }, { name: "Moroni", age: 50 }, { name: "Moroni", age: 50 }, { name: "Moroni", age: 50 }, { name: "Moroni", age: 50 }, { name: "Moroni", age: 50 }, { name: "Moroni", age: 50 }, { name: "Moroni", age: 50 }, { name: "Moroni", age: 50 }];
 
-    vm.table = new NgTableParams({
-      sorting: { name: "asc" } 
-    }, {         
+  vm.table = new NgTableParams({
+    sorting: { name: "asc" }
+  }, {
       counts: [],
       paginationMaxBlocks: 13,
       paginationMinBlocks: 2,
-      dataset: data});
+      dataset: data
+    });
 
-    vm.changeScreen = function(url: string)
-    {
-      $state.go('layout.' + url);
-    }
+  vm.changeScreen = function (url: string) {
+    $state.go('layout.' + url);
+  }
 
-    vm.logout = function(){
-      delete $sessionStorage.JWTtoken;
-      vm.JWTtoken = "";
-      $state.go('layout.login'); 
-    }
+  vm.logout = function () {
+    delete $sessionStorage.JWTtoken;
+    vm.JWTtoken = "";
+    $state.go('layout.login');
+  }
 
 
 }
