99d28257d4448b91c06be27630f69e4e5c5374ff
Andres <afmappe@hotmail.com>
2017-11-06 17:02:16 -0500

    pantalla home del administrador

---
+++ src/app/home/components/admin_home/admin_home.component.ts
@@ -0,0 +1,7 @@
+import { AdminHomeController } from './admin_home.controller';
+
+export const AdminHomeComponent: angular.IComponentOptions = {
+  templateUrl: 'app/home/components/admin_home/admin_home.html',
+  controller: AdminHomeController,
+  controllerAs: 'vm'
+};
--- src/app/home/home.routes.ts
+++ src/app/home/home.routes.ts
@@ -1,7 +1,11 @@
 /** @ngInject */
 export default function routes($stateProvider: angular.ui.IStateProvider,
-                       $urlRouterProvider: angular.ui.IUrlRouterProvider) {
+  $urlRouterProvider: angular.ui.IUrlRouterProvider) {
   $stateProvider
+    .state('layout.adminHome', {
+      url: 'adminhome',
+      template: '<admin_home></admin_home>'
+    })
     .state('layout.offerentHome', {
       url: 'offerenthome',
       template: '<offerent_home></offerent_home>'
