cf84c605efc7aae5fdde0c3b7f201aea415e78f9
Andres <afmappe@hotmail.com>
2017-11-17 22:24:27 -0500

    Modal de recuperar contraseña

---
+++ src/app/common/components/password_recovery/recovery.component.ts
@@ -0,0 +1,7 @@
+import { RecoveryController } from './recovery.controller';
+
+export const NotificationComponent: angular.IComponentOptions = {
+    templateUrl: 'app/common/components/password_recovery/recovery.html',
+    controller: RecoveryController,
+    controllerAs: 'vm'
+};
---
+++ src/app/common/components/password_recovery/recovery.controller.ts
@@ -0,0 +1,12 @@
+
+/** @ngInject */
+export function RecoveryController($scope: any,
+    toastr: any,
+    $translate: ng.translate.ITranslateService,
+    $timeout: ng.ITimeoutService,
+    $state: ng.ui.IStateService,
+    $window: any,
+    $sessionStorage: any) {
+
+    var vm = this;
+}
